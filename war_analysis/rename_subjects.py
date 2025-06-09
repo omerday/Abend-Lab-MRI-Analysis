be84a3648fadec39906a4e59e904173978c3389a
Omer <omer.dayan4u@gmail.com>
2025-06-09 19:23:25 +0300

    Allow providing the rename script with a custom prefix

--- war_analysis/rename_subjects.py
+++ war_analysis/rename_subjects.py
@@ -8,9 +8,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("subject", help="Subject ID, in the format of sub-xx")
+parser.add_argument("--prefix", help="Prefix to add before ID, eg. sub-<pref>xx")
 args = parser.parse_args()
 
 subject = args.subject
+prefix = args.prefix
+
 if not subject:
     print("No subject provided. Quitting.")
     quit()
@@ -19,20 +22,27 @@ if not subject.startswith("sub-"):
     print("Wrong subject format. Should be sub-xx. Quitting.")
     quit()
 
+if not prefix:
+    print("No prefix provided. Quitting.")
+    quit()
+
 if subject not in os.listdir():
     print(f"folder {subject} not found.")
 
 old_id = subject.split('-')[-1]
-new_subject = "".join(["sub-", "1", old_id])
+new_subject = "".join(["sub-", prefix, old_id])
 
 os.rename(subject, new_subject)
 sessions = [ses for ses in os.listdir(new_subject) if "ses-" in ses]
-folders = ['anat', 'func', 'fmap', 'dwi']
+folders = ['anat', 'func', 'fmap', 'dwi', 'anat_warped']
 
 for session in sessions:
     print(f"Going over session {session}")
     for folder in folders:
         if folder in os.listdir(f"{new_subject}/{session}"):
+            if folder == 'anat_warped':
+                os.rmdir(f"{new_subject}/{session}/{folder}")
+                continue
             print(f"Going over folder {folder}")
             path = "".join([new_subject, '/', session, '/', folder])
             for file in os.listdir(path):
